ebc88ea27e3e207bc7c8ff566b45e69ce61e6c00
Felipe Vieira Baehr <felipebaehr@gmail.com>
2024-11-27 11:26:53 -0300

    feat: add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-import { UserButton } from '@clerk/nextjs'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
+import Navbar from './_components/navbar'
 
 const HomePage = async () => {
   const { userId } = await auth()
   if (!userId) {
     redirect('/login')
   }
-  return (
-    <div>
-      <UserButton showName />
-    </div>
-  )
+  return <Navbar />
 }
 
 export default HomePage
