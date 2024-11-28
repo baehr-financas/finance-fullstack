395a16a183299a7d2dd4d522cf1d7c2a622347bc
Felipe Vieira Baehr <felipebaehr@gmail.com>
2024-11-28 10:33:18 -0300

    refactor: change language and update a env.example

--- app/_components/ui/data-table.tsx
+++ app/_components/ui/data-table.tsx
@@ -68,7 +68,7 @@ export function DataTable<TData, TValue>({
                 colSpan={columns.length}
                 className="h-24 text-center"
               >
-                No results.
+                Sem resultado.
               </TableCell>
             </TableRow>
           )}
